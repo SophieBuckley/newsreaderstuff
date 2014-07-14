bd652429250bd5720516229fa42b2f1cb5d3f270
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-14 15:23:43 +0100

    Replace 'Hello' with text in textbox

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $("p").text("Hello!");
+  $("input[type=submit]").click(function(event) {
+     event.preventDefault();
+     $("p").text($("input[type=text]").val());
+   });
 });
