4366198f97113ca5531ec6067c6faf1550a0200a
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-15 15:18:34 +0100

    Show results of searching for named actor as a table

--- code.js
+++ code.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-  $("p").text("Welcome!");
+  $("p").text("Welcome! Please enter the name of the actor you'd like to search for.");
+  var initUrl = "https://newsreader.scraperwiki.com/summary_of_events_with_actor?uris.0=dbpedia:Thierry_Henry"
   $("input[type=submit]").click(function(event) {
      event.preventDefault();
-    // $("p").text($("input[type=text]").val());
-     $("a").each(function() {
-           $(this).attr("href", function(index, old) {
-                return old.replace("Thierry_Henry", $("input[type=text]").val());
-         });
+     actor = $("input[type=text]").val();
+     actor = actor.replace(" ", "_");
+     actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
+                return letter.toUpperCase();
+            });
+     var queryUrl = initUrl.replace("Thierry_Henry", actor);
+     $.ajax({
+           url: queryUrl,
+           cache: false
+     })
+       .done(function(html) {
+               var table = $(html).find("table");
+               $("#results").html(table);
       });
    });
 });
