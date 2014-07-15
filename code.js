278373b39123df80e3f8a3287b9af94e3371257e
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-15 16:54:44 +0100

    Added pagination - need to account for clicking 'Next'

--- code.js
+++ code.js
@@ -1,21 +1,36 @@
 $(document).ready(function() {
-  $("p").text("Welcome! Please enter the name of the actor you'd like to search for.");
-  var initUrl = "https://newsreader.scraperwiki.com/summary_of_events_with_actor?uris.0=dbpedia:Thierry_Henry"
-  $("input[type=submit]").click(function(event) {
-     event.preventDefault();
-     actor = $("input[type=text]").val();
-     actor = actor.replace(" ", "_");
-     actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
-                return letter.toUpperCase();
-            });
-     var queryUrl = initUrl.replace("Thierry_Henry", actor);
-     $.ajax({
-           url: queryUrl,
-           cache: false
-     })
-       .done(function(html) {
-               var table = $(html).find("table");
-               $("#results").html(table);
-      });
-   });
+    $("p").text("Welcome! Please enter the name of the actor you'd like to search for.");
+    $("input[type=submit]").click(function(event) {
+        event.preventDefault();
+        actor = $("input[type=text]").val();
+        actor = actor.replace(" ", "_");
+        actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
+           return letter.toUpperCase();
+        });
+        runQuery(actor, "1");
+    });
 });
+
+var runQuery = function(actor, pageNum) {
+    var initUrl = "https://newsreader.scraperwiki.com/summary_of_events_with_actor/page/{0}?uris.0=dbpedia:{1}"
+    var queryUrl = initUrl.replace("{0}", pageNum);
+    var queryUrl = queryUrl.replace("{1}", actor);
+    console.log(queryUrl);
+    $.ajax({
+      url: queryUrl,
+      cache: false
+    })
+    .done(function(html) {
+        var table = $(html).find("table");
+        var pagination = $(html).find(".pagination");
+        $("#results").html("");
+        $("#results").append(pagination[0]);
+        $("#results").append(table);
+        $("#results").append(pagination[1]);
+        $(".pagination a").click(function(event) {
+            event.preventDefault();
+            nextPageNum = $(this).html()
+            runQuery(actor, nextPageNum);
+        });
+    });
+}
