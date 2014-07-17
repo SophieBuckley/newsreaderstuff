23a8cb7db161d1698af5ca9722cadeecb5fb31ae
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-17 16:29:12 +0100

    Opens results in new tab/window, may open in same page at some point

--- code.js
+++ code.js
@@ -1,36 +1,24 @@
 $(document).ready(function() {
-    $("p").text("Welcome! Please enter the name of the actor you'd like to search for.");
-    $("input[type=submit]").click(function(event) {
+    $("p").text("Welcome! Please enter the name of the actor you'd like to search for."); 
+    $("form").submit(function(event) {
         event.preventDefault();
         actor = $("input[type=text]").val();
         actor = actor.replace(" ", "_");
+        //http://regex101.com/#javascript sophie buckley --> Sophie_Buckley, accounts for accented letters (http://stackoverflow.com/questions/7874626/how-to-capitalize-each-word-even-after-hyphens-with-jquery)
         actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
            return letter.toUpperCase();
         });
-        runQuery(actor, "1");
-    });
+        navUrl(actor, "1");
+     });
 });
 
-var runQuery = function(actor, pageNum) {
+var navUrl = function move(actor, pageNum) { 
     var initUrl = "https://newsreader.scraperwiki.com/summary_of_events_with_actor/page/{0}?uris.0=dbpedia:{1}"
     var queryUrl = initUrl.replace("{0}", pageNum);
     var queryUrl = queryUrl.replace("{1}", actor);
     console.log(queryUrl);
-    $.ajax({
-      url: queryUrl,
-      cache: false
-    })
-    .done(function(html) {
-        var table = $(html).find("table");
-        var pagination = $(html).find(".pagination");
-        $("#results").html("");
-        $("#results").append(pagination[0]);
-        $("#results").append(table);
-        $("#results").append(pagination[1]);
-        $(".pagination a").click(function(event) {
-            event.preventDefault();
-            nextPageNum = $(this).html()
-            runQuery(actor, nextPageNum);
-        });
-    });
+    // http://stackoverflow.com/questions/8970600/how-to-navigate-to-a-different-page-with-javascript
+    // http://stackoverflow.com/questions/5141910/javascript-location-href-to-open-in-new-window-tab
+    // "_blank" is what makes it open in a new window
+    window.open(queryUrl, "_blank"); 
 }
