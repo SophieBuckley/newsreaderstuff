bf6ed707f167f0aadba2ef30acb7086942e45fa6
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-18 14:12:52 +0100

    Limit & offset parameters, also drop down list of query types

--- code.js
+++ code.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-    $("p").text("Welcome! Please enter the name of the actor you'd like to search for."); 
+$(document).ready(function() { 
     $("form").submit(function(event) {
         event.preventDefault();
-        actor = $("input[type=text]").val();
+        //":text" == "input[type=text]", seprate variable so that [0],[1], etc can be used to account for multiple input boxes
+        input = $(":text");
+        actor = $(input[0]).val();
         actor = actor.replace(" ", "_");
         //http://regex101.com/#javascript sophie buckley --> Sophie_Buckley, accounts for accented letters (http://stackoverflow.com/questions/7874626/how-to-capitalize-each-word-even-after-hyphens-with-jquery)
         actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
            return letter.toUpperCase();
-        });
+         });
         navUrl(actor, "1");
      });
 });
@@ -16,9 +17,10 @@ var navUrl = function move(actor, pageNum) {
     var initUrl = "https://newsreader.scraperwiki.com/summary_of_events_with_actor/page/{0}?uris.0=dbpedia:{1}"
     var queryUrl = initUrl.replace("{0}", pageNum);
     var queryUrl = queryUrl.replace("{1}", actor);
-    console.log(queryUrl);
+    limit = $(input[1]).val();
+    offset = $(input[2]).val();
+    var queryUrl = (queryUrl + ("&limit=" + limit)) + ("&offset=" + offset);
     // http://stackoverflow.com/questions/8970600/how-to-navigate-to-a-different-page-with-javascript
-    // http://stackoverflow.com/questions/5141910/javascript-location-href-to-open-in-new-window-tab
-    // "_blank" is what makes it open in a new window
-    window.open(queryUrl, "_blank"); 
+    // http://stackoverflow.com/questions/5141910/javascript-location-href-to-open-in-new-window-tab 
+    window.location = queryUrl; 
 }
