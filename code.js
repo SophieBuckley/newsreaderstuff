653adae4a5831871c2bc48ab6539aa561f080a53
Sophie Buckley <sophiebuckley08@live.co.uk>
2014-07-29 14:17:10 +0100

    Added if statement for most query types

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         //":text" == "input[type=text]", seprate variable so that [0],[1], etc can be used to account for multiple input boxes
         textInput = $(":text");
         actor = $(textInput[0]).val();
+        //actor2 = $(textInput[1]).val();
         actor = actor.replace(" ", "_");
         //http://regex101.com/#javascript sophie buckley --> Sophie_Buckley, accounts for accented letters (http://stackoverflow.com/questions/7874626/how-to-capitalize-each-word-even-after-hyphens-with-jquery)
         actor = actor.toLowerCase().replace(/(^([a-zA-Z\p{M}]))|([ _][a-zA-Z\p{M}])/g, function(letter) {
@@ -14,23 +15,33 @@ $(document).ready(function() {
 });
 
 var runQuery = function move(actor, pageNum) { 
-    var initUrl = "https://newsreader.scraperwiki.com/{0}/page/{1}?uris.0={2}"
-    var queryUrl = initUrl.replace("{1}", pageNum);
-    var queryUrl = queryUrl.replace("{2}", "dbpedia:" + actor);
-    limit = $(textInput[1]).val();
-    offset = $(textInput[2]).val();
-    stringFilter = $(textInput[3]).val();
-    dateFilter = $(textInput[4]).val();
-    framenet = $(textInput[5]).val();
+    initUrl = "https://newsreader.scraperwiki.com/{0}/page/{1}?uris.0={2}"
+    queryUrl = initUrl.replace("{1}", pageNum);
+    limit = $(textInput[2]).val();
+    offset = $(textInput[3]).val();
+    stringFilter = $(textInput[4]).val();
+    dateFilter = $(textInput[5]).val();
+    framenet = $(textInput[6]).val();
     if (limit != "") {queryUrl += ("&limit=" + limit)};
     if (offset != "") {queryUrl += ("&offset=" + offset)};
     if (stringFilter != "") {queryUrl += ("&filter=" + stringFilter)};
     if (dateFilter != "") {queryUrl += ("&datefilter=" + dateFilter)};
     listInput = $("select");    
-    outputFormat = $(listInput[0]).val();
+    outputFormat = $(listInput[1]).val();
     queryUrl = queryUrl + ("&output=" + outputFormat);
-    queryType = $(listInput[1]).val();
+    queryType = $(listInput[0]).val();
     queryUrl = queryUrl.replace("{0}", queryType);
+    if (queryType == "describe_uri" || queryType == "event_details_filtered_by_actor" || queryType == "people_sharing_event_with_a_person" || queryType == "summary_of_events_with_actor") {
+        queryUrl = queryUrl.replace("{2}", "dbpedia:" + actor);
+    } else if (queryType == "properties_of_a_type" || queryType == "summary_of_events_with_actor_type") {
+        queryUrl = queryUrl.replace("{2}", "dbo:" + actor);
+    } else if (queryType == "get_document" || queryType == "get_document_metadata" || queryType == "get_mention_metadata") {
+        queryUrl = queryUrl.replace("{2}", link);
+    } else if (queryType == "summary_of_events_with_framenet") {
+        queryUrl = queryUrl.replace("{2}", "framenet:" + framenet);
+    } else {
+    }
+    console.log(queryUrl);
     if (outputFormat == "html") {
     //display the results of the query in a table underneath the submit button
         $.ajax({
